e01518d50d31eea722ede4183c759ae5d662e1a9
Claudio Inacio <inacio.unipar@gmail.com>
2021-03-04 23:25:00 -0300

    testando commit

---
+++ src/aplicacao/Projeto.java
@@ -0,0 +1,18 @@
+package aplicacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Projeto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+			
+		System.out.println("hello word!!");
+		
+		sc.close();
+	}
+
+}
